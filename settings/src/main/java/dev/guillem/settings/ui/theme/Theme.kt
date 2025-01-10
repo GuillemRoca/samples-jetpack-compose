048a94e61bb06054f77307b72f57c2b0d77a946a
Guillem Roca <10449477+GuillemRoca@users.noreply.github.com>
2025-01-10 14:23:51 +0100

    refactor: Update theme in Settings module
    - Removes the custom theme style (`Theme.Samplesjetpackcompose`).
    - Updates the AndroidManifest.xml to use the default Material theme.
    - Removes unused imports and code related to theming in SettingsActivity.

--- settings/src/main/java/dev/guillem/settings/ui/theme/Theme.kt
+++ settings/src/main/java/dev/guillem/settings/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package dev.guillem.settings.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
