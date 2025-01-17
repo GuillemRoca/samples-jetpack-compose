2cd0cb4c37a6c89748bc0f4d4d62e6d5915366d7
Guillem Roca <10449477+GuillemRoca@users.noreply.github.com>
2025-01-17 16:24:15 +0100

    test: Setup Compose Settings tests - 15
    - Removes the previous unit and instrumented tests.
    - Adds a new instrumented test class for Compose using `createComposeRule`.
    - Updates dependencies in `build.gradle.kts` to include Compose testing libraries and remove JUnit and Espresso dependencies.
    - Updates `libs.versions.toml` with new dependencies versions.

---
+++ settings/src/androidTest/java/dev/guillem/settings/SettingsTest.kt
@@ -0,0 +1,10 @@
+package dev.guillem.settings
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import org.junit.Rule
+
+class SettingsTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+}
