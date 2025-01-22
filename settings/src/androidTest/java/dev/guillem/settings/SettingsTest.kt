67eda19d520eb6663dc8fb99e073d472e0480592
Guillem Roca <10449477+GuillemRoca@users.noreply.github.com>
2025-01-22 15:37:56 +0100

    test: Add test for enabling notifications
    - Adds a test to verify that clicking on the "Enabled Notifications" setting toggles its selected state.
    - Adds a test tag to the toggle item for identification in tests.
    - Adds a Tags object to store test tags.

--- settings/src/androidTest/java/dev/guillem/settings/SettingsTest.kt
+++ settings/src/androidTest/java/dev/guillem/settings/SettingsTest.kt
@@ -2,9 +2,13 @@ package dev.guillem.settings
 
 import androidx.annotation.StringRes
 import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertIsOn
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
 import androidx.test.platform.app.InstrumentationRegistry
+import dev.guillem.settings.Tags.TAG_TOGGLE_ITEM
 import org.junit.Rule
 import org.junit.Test
 
@@ -46,5 +50,19 @@ class SettingsTest {
         composeTestRule.onNodeWithText(
             InstrumentationRegistry.getInstrumentation().targetContext.getString(title)
         ).assertIsDisplayed()
+    }
+
+    @Test
+    fun Enable_Notifications_Toggles_Selected_State() {
+        composeTestRule.setContent {
+            Settings()
+        }
+
+        composeTestRule.onNodeWithText(
+            InstrumentationRegistry.getInstrumentation().targetContext.getString(R.string.setting_enabled_notifications)
+        ).performClick()
+
+        composeTestRule.onNodeWithTag(TAG_TOGGLE_ITEM)
+            .assertIsOn()
     }
 }
---
+++ settings/src/main/java/dev/guillem/settings/Tags.kt
@@ -0,0 +1,5 @@
+package dev.guillem.settings
+
+object Tags {
+    const val TAG_TOGGLE_ITEM = "toggle_item"
+}
