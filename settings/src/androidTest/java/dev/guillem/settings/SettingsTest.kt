c466c73ab1c87849fa6ed38534b16dc0d606f7a1
Guillem Roca <10449477+GuillemRoca@users.noreply.github.com>
2025-01-22 15:46:51 +0100

    test: Add UI tests for Marketing options - 19
    - Adds UI tests to verify the behavior of Marketing options in the Settings screen.
    - Adds test tags to Marketing option components for easier identification in tests.
    - Updates existing tests to improve readability and maintainability.

--- settings/src/androidTest/java/dev/guillem/settings/SettingsTest.kt
+++ settings/src/androidTest/java/dev/guillem/settings/SettingsTest.kt
@@ -3,12 +3,14 @@ package dev.guillem.settings
 import androidx.annotation.StringRes
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertIsOn
+import androidx.compose.ui.test.assertIsSelected
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.test.platform.app.InstrumentationRegistry
 import dev.guillem.settings.Tags.TAG_CHECK_ITEM
+import dev.guillem.settings.Tags.TAG_MARKETING_OPTION
 import dev.guillem.settings.Tags.TAG_TOGGLE_ITEM
 import org.junit.Rule
 import org.junit.Test
@@ -79,5 +81,19 @@ class SettingsTest {
 
         composeTestRule.onNodeWithTag(TAG_CHECK_ITEM)
             .assertIsOn()
+    }
+
+    @Test
+    fun Marketing_Options_Toggles_Selected_State() {
+        composeTestRule.setContent {
+            Settings()
+        }
+
+        composeTestRule.onNodeWithText(
+            InstrumentationRegistry.getInstrumentation().targetContext.resources.getStringArray(R.array.settings_option_marketing_choice)[1]
+        ).performClick()
+
+        composeTestRule.onNodeWithTag(TAG_MARKETING_OPTION + 1)
+            .assertIsSelected()
     }
 }
--- settings/src/main/java/dev/guillem/settings/Tags.kt
+++ settings/src/main/java/dev/guillem/settings/Tags.kt
@@ -3,4 +3,6 @@ package dev.guillem.settings
 object Tags {
     const val TAG_TOGGLE_ITEM = "toggle_item"
     const val TAG_CHECK_ITEM = "check_item"
+    const val TAG_MARKETING_OPTION = "marketing_option_"
+
 }
