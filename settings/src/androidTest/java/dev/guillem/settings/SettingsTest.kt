02be8dbd1d69c252c67d563583c4b3238ad017a7
Guillem Roca <10449477+GuillemRoca@users.noreply.github.com>
2025-01-22 15:30:27 +0100

    test: Add UI tests for Settings screen
    Adds UI tests for the Settings screen to verify that all settings are displayed correctly.
    
    - Adds tests for each setting to check if it is displayed.
    - Uses `onNodeWithText` and `assertIsDisplayed` to assert the visibility of settings.
    - Refactors the test code to use a common function for asserting settings.

--- settings/src/androidTest/java/dev/guillem/settings/SettingsTest.kt
+++ settings/src/androidTest/java/dev/guillem/settings/SettingsTest.kt
@@ -1,10 +1,50 @@
 package dev.guillem.settings
 
+import androidx.annotation.StringRes
+import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.test.platform.app.InstrumentationRegistry
 import org.junit.Rule
+import org.junit.Test
 
 class SettingsTest {
 
     @get:Rule
     val composeTestRule = createComposeRule()
+
+    @Test
+    fun Enable_Notifications_Settting_Is_Displayed() {
+        assertSettingIsDisplayed(R.string.setting_enabled_notifications)
+    }
+
+    @Test
+    fun Show_Hints_Settting_Is_Displayed() {
+        assertSettingIsDisplayed(R.string.setting_show_hints)
+    }
+
+    @Test
+    fun Manage_Subscription_Settting_Is_Displayed() {
+        assertSettingIsDisplayed(R.string.setting_manage_subscription)
+    }
+
+    @Test
+    fun Option_Theme_Settting_Is_Displayed() {
+        assertSettingIsDisplayed(R.string.setting_option_theme)
+    }
+
+    @Test
+    fun App_Version_Settting_Is_Displayed() {
+        assertSettingIsDisplayed(R.string.setting_app_version)
+    }
+
+    private fun assertSettingIsDisplayed(@StringRes title: Int) {
+        composeTestRule.setContent {
+            Settings()
+        }
+
+        composeTestRule.onNodeWithText(
+            InstrumentationRegistry.getInstrumentation().targetContext.getString(title)
+        ).assertIsDisplayed()
+    }
 }
